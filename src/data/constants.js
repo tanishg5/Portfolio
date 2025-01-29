80ddec462dbc6f849743d5be21221237859a3faf
Shikhar Singhal <shikhar@Shikhars-MacBook-Pro-2.local>
2025-01-30 02:09:48 +0530

    project portfolio setup

---
+++ src/data/constants.js
@@ -0,0 +1,174 @@
+export const Bio = {
+  name: "Tanish Gupta",
+  roles: [
+    "Aspiring Software Engineer",
+    "Competitive Programmer",
+    "Computer Science student"
+  ],
+  description:
+    "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to Programming. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
+  github: "https://github.com/tanishg5",
+  resume:
+    "https://drive.google.com/file/d/1N96wDdXQJGMKQVIcr10PQF9jHfaRc6PD/view?usp=drive_link",
+  linkedin: "linkedin.com/in/tanish-gupta-b78a23238",
+  // twitter: "https://twitter.com/RishavChanda",
+  // insta: "https://www.instagram.com/rishav_chanda/",
+  // facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      { 
+        name:"Tailwind Css",
+        image:"https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/tailwind-css-icon.png",
+      }
+     
+    ],
+  },
+  {
+    title: "Coursework",
+    skills: [
+      {
+        name: " Data Structures and Algorithms",
+      },
+      {
+        name: "DBMS",
+      },
+      {
+        name: "OOPS",
+      },
+      {
+        name: "Computer Architecture",
+      },
+      {
+        name: "Operating Systems",
+      },
+    ],
+  },
+  // {
+  //   title: "Android",
+  //   skills: [
+  //     {
+  //       name: "Java",
+  //       image:
+  //         "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+  //     },
+  //     {
+  //       name: "Kotlin",
+  //       image:
+  //         "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+  //     },
+  //     {
+  //       name: "Jetpack Compose",
+  //       image:
+  //         "https://3.bp.blogspot.com/-VVp3WvJvl84/X0Vu6EjYqDI/AAAAAAAAPjU/ZOMKiUlgfg8ok8DY8Hc-ocOvGdB0z86AgCLcBGAsYHQ/s1600/jetpack%2Bcompose%2Bicon_RGB.png",
+  //     },
+  //     {
+  //       name: "XML",
+  //       image:
+  //         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBMw6_RdwKQ9bDFfnKDX1iwMl4bVJEvd9PP53XuIw&s",
+  //     },
+  //     {
+  //       name: "Android Studio",
+  //       image:
+  //         "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  //     },
+  //   ],
+  // },
+  
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+    ],
+  },
+];
+
+
+export const education = [
+  {
+    id: 0,
+    college: "JECRC University , Jaipur",
+    date: "SEP 2022 - Present",
+    grade: "8.8  CGPA",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  }
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Book Library",
+
+    description:
+      "A Rule Builder application “Book Library”  using React involves creating components to display a list of books, adding functionality to manage the library, and possibly integrating with a backend for persistent data storage..",
+   
+    tags: [
+      "React Js",
+      "RestFul API Integration",
+      "Css",
+      "Redux",
+      "React Flow",
+    ],
+    webapp: "https://book-library-e01fe1.netlify.app/",
+  },
+  {
+    id: 9,
+    title: "Pincode-FoodDelivery-App",
+    description:
+      "Pincode provides an excellent user experience by dynamically updating the content on the page and offering real-time information on restaurants, banners, offers, menus and availability to keep the users all engaged with the latest offers.",
+
+    tags: [
+      "ReactJs",
+      "Redux",
+      "Tailwind-CSS",
+      "Eslint",
+      "Lazy Loading & Code Splitting",
+      "Shimmer UI"
+      
+      
+    ],
+    webapp: "https://pincode-delivery-app.netlify.app",
+  },
+  
+ 
+  
+];
+
+
