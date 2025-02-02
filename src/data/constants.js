94f2ece3bbd7e7f93bec704ab38d70479ef5e511
Tanish Gupta <55tanishqgupta@gmail.com>
2025-02-03 01:05:17 +0530

    change in constant files

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to Programming. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/tanishg5",
   resume:
-    "https://drive.google.com/file/d/1N96wDdXQJGMKQVIcr10PQF9jHfaRc6PD/view?usp=drive_link",
+    "https://drive.google.com/file/d/1_rQ4GjU7k9X0sDCD3hPojHuOhia_1x7j/view?usp=drivesdk",
   linkedin: "linkedin.com/in/tanish-gupta-b78a23238",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
